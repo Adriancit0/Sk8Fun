786746ee2c4be605551ad42d3f937c144675c662
Adriancit0 <adrianruanol@gmail.com>
2021-06-10 16:32:21 +0200

    Rework school details update school model

--- frontend/src/components/schoolDetails/index.jsx
+++ frontend/src/components/schoolDetails/index.jsx
@@ -8,7 +8,7 @@ function schoolDetail() {
   const { schoolId } = useParams();
   const dispatch = useDispatch();
   const school = useSelector((store) => store.itemSelected);
-  // const { activities } = school;
+  const { activities } = school;
   useEffect(() => {
     dispatch(getById(schoolId));
   }, []);
@@ -17,24 +17,54 @@ function schoolDetail() {
     <section className="school-detail">
       <section className="school-detail__school-card">
         <h3>{school.info?.name}</h3>
-        <ul className="school-card__contact">
-          <h4>Contact</h4>
-          <li>
-            Phone:
-            {' '}
-            {school?.info?.phone}
+        <section className="school-card__school-info">
+          <img className="school-info__image" src={school?.info?.imageUrl} alt="skatepark" />
+          <ul className="school-info__contact">
+            <h4>Contact</h4>
+            <li>
+              Phone:
+              {' '}
+              {school?.info?.phone}
+            </li>
+            <li>
+              Mail:
+              {' '}
+              {school?.info?.mail}
+            </li>
+            <li>
+              Address:
+              {' '}
+              {school?.info?.address}
+            </li>
+          </ul>
+        </section>
+        <ul className="school-card__activities-list" />
+        {activities?.map((activity) => (
+          <li key={activity?._id} className="activities-list__activitie-item">
+            <ul>
+              <h4>
+                {activity?.description}
+              </h4>
+              <li>
+                Level:
+                {' '}
+                {activity?.level}
+              </li>
+              <li>
+                Horario:
+                {' '}
+                {activity?.schedule}
+              </li>
+              <li>
+                Price:
+                {' '}
+                {activity?.price?.quantity}
+                /
+                {activity?.price?.unity}
+              </li>
+            </ul>
           </li>
-          <li>
-            Mail:
-            {' '}
-            {school?.info?.mail}
-          </li>
-          <li>
-            Adress:
-            {' '}
-            {school?.info?.adress}
-          </li>
-        </ul>
+        ))}
       </section>
     </section>
   );
