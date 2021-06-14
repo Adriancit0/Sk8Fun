fa97185a19204a08fb50ab1e30a6b9d87eaff5a5
Adriancit0 <adrianruanol@gmail.com>
2021-06-14 18:03:00 +0200

    Add imInterested toogle in activitydetail component

--- frontend/src/components/activityDetail/index.jsx
+++ frontend/src/components/activityDetail/index.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable no-underscore-dangle */
-import { React } from 'react';
-
+import { React, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useParams } from 'react-router';
 // import { updateById } from '../../redux/actions/actionsCreators';
 
 function activityDetail({ activity }) {
-  return (
+  const [imInterested, setImInterested] = useState(true);
+  // const { schoolId } = useParams();
+  // const useDispatch = useDispatch();
+  // const school = useSelector((store) => store.itemSelected);
+  function handleImInterested() {
+    setImInterested(!imInterested);
+  }
 
+  return (
     <li key={activity?._id} className="activities-list__activitie-item">
       <ul>
         <h4>
@@ -32,6 +40,7 @@ function activityDetail({ activity }) {
           <button type="button">+</button>
           <button type="button">-</button>
           <button type="button">Book</button>
+          <button type="button" onClick={handleImInterested}>Im interested</button>
           <p>
             {activity?.likes}
             {' '}
