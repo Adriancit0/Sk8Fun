72327aefc205f2639d51b28fb778b20f409dd40a
Adriancit0 <adrianruanol@gmail.com>
2021-06-19 19:58:19 +0200

    Clear console.logs and debuggers

--- frontend/src/components/createActivity/index.jsx
+++ frontend/src/components/createActivity/index.jsx
@@ -1,20 +1,17 @@
-/* eslint-disable no-debugger */
 import { React, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import InputSection from '../inputSection';
 import StandardButton from '../button';
 import { updateById } from '../../redux/actions/actionsCreators';
 
 function createActivity({ schoolId }) {
-  // eslint-disable-next-line no-debugger
-
   const school = useSelector((store) => store.itemSelected);
   let { activities } = school;
   const dispatch = useDispatch();
 
   const [description, setDescription] = useState('');
   const [level, setLevel] = useState('Iniciación');
+  const [schedule, setSchedule] = useState('');
   const [price, setPrice] = useState('');
   const [unityPrice, setUnityPrice] = useState('clase');
   const [places, setPlaces] = useState(5);
@@ -23,6 +20,7 @@ function createActivity({ schoolId }) {
     const newActivity = {
       description,
       level,
+      schedule,
       price: {
         quantity: price,
         unity: unityPrice
@@ -39,6 +37,7 @@ function createActivity({ schoolId }) {
     <form className="school-card__activity-create" onSubmit={handleCreateActivity}>
       <InputSection htmlFor="description" type="text" id="description" value={description} functionName={setDescription} content="Descripción de la actividad:" />
       <InputSection htmlFor="level" type="text" id="level" value={level} functionName={setLevel} content="Nivel al que está orientado:" />
+      <InputSection htmlFor="level" type="text" id="level" value={level} functionName={setSchedule} content="Horario de la actividad:" />
       <InputSection htmlFor="price" type="text" id="price" value={price} functionName={setPrice} content="Precio en €:" />
       <InputSection htmlFor="unityPrice" type="text" id="unityPrice" value={unityPrice} functionName={setUnityPrice} content="Unidad por pago:" />
       <InputSection htmlFor="places" type="text" id="places" value={places} functionName={setPlaces} content="Número de plazas:" />
